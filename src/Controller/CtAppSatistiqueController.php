ef07d947ca6c7bdb4b299397f648e728d6374a97
kaira <husaynmozart@gmail.com>
2024-02-16 13:23:34 +0300

    avancement mutation, preparation imprimer technique et statistique

--- src/Controller/CtAppSatistiqueController.php
+++ src/Controller/CtAppSatistiqueController.php
@@ -6,9 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/ct_app_statistique")
+ */
 class CtAppSatistiqueController extends AbstractController
 {
-    #[Route('/ct/app/satistique', name: 'app_ct_app_satistique')]
+    //#[Route('/ct/app/satistique', name: 'app_ct_app_satistique')]
+    /**
+     * @Route("/", name="app_ct_app_statistique")
+     */
     public function index(): Response
     {
         return $this->render('ct_app_satistique/index.html.twig', [
